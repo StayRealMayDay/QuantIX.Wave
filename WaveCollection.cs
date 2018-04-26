aa9eff2d3eea568746c51b132d02dca6150dcf11
renhaoran <RHRxjtu@outlook.com>
2018-04-26 19:02:04 +0800

    use char to indicate the model

--- WaveCollection.cs
+++ WaveCollection.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using QuantTC.Data;
 using QuantTC.Indicators;
 using QuantTC.Indicators.Generic;
 using static QuantTC.X;
+
 namespace QuantIX.Wave
 {
     public class WaveCollection
     {
-        public WaveCollection(IIndicator<IPrice> source, int cycle)
+        public WaveCollection(IIndicator<IPrice> source, int cycle, double threshold)
         {
             Source = source;
             Cycle = cycle;
+            Threshold = threshold;
             OldPosition = -1;
             Close = Source.LiveSelect((arg, i) => arg.Close);
             TenSMA = Close.SMA(10);
@@ -20,7 +24,7 @@ namespace QuantIX.Wave
         }
 
 
-        public void IdentifyWaves()
+        private void IdentifyWaves()
         {
             for (int i = 0; i < SixtySMA.Count; i++)
             {
@@ -31,6 +35,7 @@ namespace QuantIX.Wave
                     {
                         Wave.Add(new Wave(OldPosition, i, SixtySMA[OldPosition], SixtySMA[i], 1));
                     }
+
                     OldPosition = i;
                 }
 
@@ -40,13 +45,60 @@ namespace QuantIX.Wave
                     {
                         Wave.Add(new Wave(OldPosition, i, SixtySMA[OldPosition], SixtySMA[i], -1));
                     }
+
                     OldPosition = i;
                 }
             }
         }
-        
-        
-        
+
+
+        private void CreateTheSequence()
+        {
+            for (int i = 1; i < Wave.Count; i++)
+            {
+                WaveSequence.Add(CompareWave(Wave[i - 1], Wave[i], Threshold));
+            }
+        }
+
+        private char CompareWave(Wave wave1, Wave wave2, double threshold)
+        {
+            if (wave1.Tendency != wave2.Tendency)
+            {
+                if (CompareSlpoe(wave1.Slope, wave2.Slope, threshold))
+                {
+                    var compareLength = wave2.Length / wave1.Length;
+                    if (compareLength > 1.5)
+                    {
+                        return 'L';
+                    }
+
+                    if (compareLength < 0.5)
+                    {
+                        return 'j';
+                    }
+
+                    return 'v';
+                }
+
+                return 'y';
+            }
+
+            return 'i';
+        }
+
+        private bool CompareSlpoe(double wave1Slope, double wave2Slope, double threshold)
+        {
+            var Slope = Math.Min(wave1Slope, wave2Slope) / Math.Max(wave1Slope, wave2Slope);
+            return Slope > threshold;
+        }
+
+
+        public List<char> GetData()
+        {
+            IdentifyWaves();
+            CreateTheSequence();
+            return WaveSequence;
+        }
 
         public bool IsALowPoint => RangeR(0, (Idx + Cycle) > SixtySMA.Count ? SixtySMA.Count : Idx + Cycle)
             .Take(Cycle * 2)
@@ -56,8 +108,9 @@ namespace QuantIX.Wave
             .Take(Cycle * 2)
             .All(j => SixtySMA[j] <= SixtySMA[Idx]);
 
-
-        private List<int> WaveSequence { get; set; }
+        private double Threshold { get; set; }
+        
+        private List<char> WaveSequence { get; set; }
 
         private int OldPosition { get; set; }
 
@@ -68,12 +121,11 @@ namespace QuantIX.Wave
         public SMA TenSMA { get; set; }
 
         private int Idx { set; get; }
-    
+
         public LiveSelector<IPrice, double> Close { get; set; }
 
         public int Cycle { get; set; }
 
         public IIndicator<IPrice> Source { get; set; }
-
     }
 }
